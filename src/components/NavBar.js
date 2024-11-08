c2ff2861757819027f5e26e39394783a85cb0414
Eyiladeogo <adedayoeyiladeogo22@gmail.com>
2024-11-08 17:55:00 +0100

    Navbar responsiveness completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import bounceLogo from '../assets/icon.svg'
 import SignIn from './SignIn';
 import UserProfile from './UserProfile';
 import { Link, useNavigate } from 'react-router-dom'; // Assuming you are using react-router for navigation
-import { FaShoppingCart, FaHeart, FaUser, FaSearch } from 'react-icons/fa'; // You can replace these with your icons
+import { FaShoppingCart, FaHeart, FaUser, FaSearch, FaBars } from 'react-icons/fa'; // You can replace these with your icons
 import { useSelector, useDispatch } from 'react-redux';
 // import { login,logout } from '../features/authSlice';
 import { setSearchQuery } from '../features/searchSlice';
@@ -38,16 +38,17 @@ const Navbar = () => {
     //     <Link to="/shop" className="shop-link">Shop</Link>
     //   </div>
 
-    <nav className="flex justify-between items-center bg-white px-12 py-4 shadow-md">
+    <nav className="flex justify-between items-center bg-white px-4 lg:px-12 py-4 shadow-md">
         <div className="flex items-center">
             {/* Logo and Shop Link */}
-            <Link to="/" className="mr-8">
-            <img src={bounceLogo} alt="Logo" className="h-10" />
+            <Link to="/" className="mr-4 lg:mr-8">
+            <img src={bounceLogo} alt="Logo" className="h-8 lg:h-10" />
             </Link>
-            <Link to="/shop" className="text-black montserrat font-bold text-lg">SHOP</Link>
+            <Link to="/shop" className="text-black montserrat font-bold text-sm lg:text-lg">SHOP</Link>
         </div>
 
-        <div className="relative mx-auto w-1/5">
+      <div className='flex-1 mx-4 lg:mx-12 max-w-md'>
+        <div className="relative">
             {/* Search Bar */}
             <input
             type="text"
@@ -64,7 +65,7 @@ const Navbar = () => {
             className="w-full p-2 border border-gray-300 montserrat rounded-full text-center"
             onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
             />
-            <FaSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500' onClick={handleSearch}/>
+            <FaSearch className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer' onClick={handleSearch}/>
             {inputValue && (
               <button onClick={() => {
                 setInputValue('')
@@ -80,9 +81,10 @@ const Navbar = () => {
 
             }
         </div>
+      </div>
 
-      <div className="hidden lg:flex gap-4 text-2*1 icons">
-        {/* Icons */}
+      {/* Main Navbar Icons */}
+      <div className="hidden lg:flex gap-4 text-xl icons">
         <Link to='/saved'>
             <FaHeart className="text-black cursor-pointer" title="Saved Items" />
         </Link>
@@ -104,12 +106,13 @@ const Navbar = () => {
       </div>
 
       {/* Menu Icon for Small Screens */}
-      <div className='lg:hidden cursor-pointer text-black' onClick={toggleMenu}>
-        <FaUser className='text-2xl'/>
+      <div className='lg:hidden cursor-pointer text-black text-2x1' onClick={toggleMenu}>
+        <FaBars className='text-2xl'/>
       </div>
 
+      {/* Mobile Dropdown Menu   */}
       {isMenuOpen &&(
-        <div className='absolute top-16 right-0 bg-white shadow-md p-4 flex flex-col items-start lg:hidden'>
+        <div className='absolute top-16 right-4 bg-white shadow-lg rounded-md p-4 flex flex-col lg:hidden'>
           <Link to='/saved' className='text-black cursor-pointer py-2' onClick={toggleMenu}>
             <FaHeart className="text-black cursor-pointer" title="Saved Items" />
           </Link>
