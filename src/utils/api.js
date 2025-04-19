9a371998d5958442d3404f334065ddc39af03930
Eyiladeogo Adedayo <adedayoeyiladeogo22@gmail.com>
2025-04-19 08:51:17 +0100

    changed baseUrl from heroku to render

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://bounce-9115b56f2109.herokuapp.com/', // Replace with your backend base URL
+  baseURL: "https://bounce-vzin.onrender.com", // Replace with your backend base URL
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 // Interceptor to add authorization token
-api.interceptors.request.use((config) => {
-  // console.log('Request Object:', config)
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Token ${token}`;
+api.interceptors.request.use(
+  (config) => {
+    // console.log('Request Object:', config)
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Token ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+);
 
 export default api;
